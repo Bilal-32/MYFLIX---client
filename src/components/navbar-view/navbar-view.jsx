143d0e06df284b34a1714abf4a0bbf135038d442
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-12 13:57:28 +0100

    Create navbar-view.jsx

---
+++ src/components/navbar-view/navbar-view.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import "./navbar-view.scss";
+
+export function Navigation({ logOut }) {
+    const user = localStorage.getItem('user');
+    const onLoggedOut = () => {
+        localStorage.clear();
+        window.open("/", "_self");
+    };
+
+    const isAuth = () => {
+        //if (typeof window == 'undefined') {
+        //  return false;
+        // }
+        if (localStorage.getItem('token')) {
+            return localStorage.getItem('token');
+        } else {
+            return false;
+        }
+    };
+
+    return (
+        <Navbar
+            id="main-nav"
+            className="mb-5"
+            bg="dark"
+            variant="dark"
+            expand="lg"
+            sticky="top"
+        >
+            {/*<Container>*/}
+            <Navbar.Brand id="appName" href="/">
+                myMovieFlix-Client
+            </Navbar.Brand>
+
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse
+                className="justify-content-end"
+                id="responsive-navbar-nav"
+            >
+                <Nav>
+                    {isAuth() && (
+                        <Nav.Link className="navbar-link" href={`/users/${user}`}>
+                            MyPage
+                        </Nav.Link>
+                    )}
+
+                    {isAuth() && (
+                        <Button
+                            className="navbar-button"
+                            variant="outline-danger"
+                            onClick={logOut}
+                        >
+                            LogOut
+                        </Button>
+                    )}
+                </Nav>
+            </Navbar.Collapse>
+            {/*</Container>*/}
+        </Navbar>
+    );
+}
+
+export default Navigation;
