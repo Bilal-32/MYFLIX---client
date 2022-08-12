3c27e84b6ee573b3e25077b98c21930c23093efb
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-12 13:54:33 +0100

    Create movie-view.jsx

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import "./movie-view.scss";
+
+export class MovieView extends React.Component {
+
+    render() {
+        const { movie, onBackClick } = this.props;
+
+        return (
+            <Container>
+                {movie && (
+                    <Row>
+                        <Col>
+                            <Card id="movie-view">
+                                <Card.Body>
+                                    <Card.Img
+                                        id="movie-view-image"
+                                        variant="top"
+                                        src={movie.ImagePath}
+                                    />
+                                    <Card.Title id="movie-Title" className="movie-title">
+                                        {movie.Title}
+                                    </Card.Title>
+                                    <Card.Text
+                                        id="movie-description"
+                                        className="movie-description"
+                                    >
+                                        {movie.Description}
+                                    </Card.Text>
+                                    <Card.Text id="movie-director" className="movie-director">
+                                        Director: {movie.Director.Name}
+                                    </Card.Text>
+                                    <Card.Text id="movie-genre" className="movie-genre">
+                                        Genre: {movie.Genre.Name}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                            <Button id="movie-view-button" onClick={() => { onBackClick(null); }}>Back</Button>
+                            <Button id="movie-view-button" onClick={() => { }}>Add to Favorites</Button>
+                        </Col>
+                    </Row>
+                )}
+            </Container>
+        );
+    }
+}
