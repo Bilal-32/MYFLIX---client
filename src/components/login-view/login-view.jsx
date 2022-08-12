2798cfaa46c2e09f6a9cdbacde6e47afb76c9e0c
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-08-12 22:31:43 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,4 @@
-mport React, { useState } from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 import axios from 'axios';
